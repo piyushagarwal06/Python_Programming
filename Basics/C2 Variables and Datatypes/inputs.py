df55544ac7bedf86a987e5e6e940b49be0331bf3
piyushagarwal06 <peeyushagerwal720@gmail.com>
2026-02-06 21:49:53 +0530

    taking user input

---
+++ Basics/C2 Variables and Datatypes/inputs.py
@@ -0,0 +1,16 @@
+# a=input("Enter number 1 ") a=33
+# b=input("Enter number 2 ") b=44
+
+# print("Number a is",a)
+# print("Number b is ",b)
+
+# print("Sum of a and b is ",(a+b)) output =3344
+
+# correct logic
+a=int(input("Enter number 1 "))
+b=int(input("Enter number 2 "))
+
+print("Number a is",a)
+print("Number b is ",b)
+
+print("Sum of a and b is ",(a+b))
