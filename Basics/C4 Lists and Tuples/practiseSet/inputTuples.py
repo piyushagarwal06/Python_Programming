81e7a308a538d3a03c3a0b9d0ca9187878ed646b
piyushagarwal06 <peeyushagerwal720@gmail.com>
2026-02-09 06:27:58 +0530

    user input in tuples

---
+++ Basics/C4 Lists and Tuples/practiseSet/inputTuples.py
@@ -0,0 +1,13 @@
+marks=[]
+f1=int(input("Enter marks for student1"))
+marks.append(f1)
+f2=int(input("Enter marks for student2"))
+marks.append(f2)
+f3=int(input("Enter marks for student3"))
+marks.append(f3)
+f4=int(input("Enter marks for student4"))
+marks.append(f4)
+f5=int(input("Enter marks for student5"))
+marks.append(f5)
+marks.sort()
+print(marks)
