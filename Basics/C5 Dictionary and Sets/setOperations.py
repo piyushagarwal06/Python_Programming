9d82b488a15083f7ff050a3e372378b93141ad4d
piyushagarwal06 <peeyushagerwal720@gmail.com>
2026-02-10 05:45:23 +0530

    set basics

---
+++ Basics/C5 Dictionary and Sets/setOperations.py
@@ -0,0 +1,4 @@
+s1={1,23,42}
+s2={7,8,1,78}
+print(s1.union(s2))
+print(s1.intersection(s2))
---
+++ Basics/C5 Dictionary and Sets/setsBasics.py
@@ -0,0 +1,8 @@
+e=set()
+s={1,2,3,4,5,5,4,3,2,1,"Harry"}
+print(s)
+
+s.add("ssp")
+s.remove(2) #removes duplicate calues as well
+
+print(s,type(s))
