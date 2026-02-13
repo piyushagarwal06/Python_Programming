b5f4a6a0db13f7a9a7d91e36b35233621aab6e65
piyushagarwal06 <peeyushagerwal720@gmail.com>
2026-02-14 04:52:21 +0530

    function argument with return

---
+++ Basics/C8 Functions/functionArgumentReturn.py
@@ -0,0 +1,11 @@
+def goodDay(name,ending):
+    print("Good Day,"+name + ending)
+    print(ending)
+    return "Shree"
+
+goodDay("Harry", " Dhanyawad")
+goodDay("Rohan" , " Dhanyawad")
+goodDay("Saswat" ," Dhanyawad")
+
+b=goodDay("Harry", "Thanks")
+print(b)
