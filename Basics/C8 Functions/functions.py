390a4d7e28c4cd29986ba843ac3883c20f7a7638
piyushagarwal06 <peeyushagerwal720@gmail.com>
2026-02-14 04:53:46 +0530

    function in python

---
+++ Basics/C8 Functions/functions.py
@@ -0,0 +1,11 @@
+#function definition 
+def avg():
+    a=int(input("Enter your number: "))
+    b=int(input("Enter your number: "))
+    c=int(input("Enter your number: "))
+
+    average=(a+b+c)/3
+    print(average)
+
+
+avg() #function call
