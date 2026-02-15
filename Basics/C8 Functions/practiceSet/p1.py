de8486d0ac2917eadee9a8c17afb0d7e65f376c2
piyushagarwal06 <peeyushagerwal720@gmail.com>
2026-02-15 07:30:52 +0530

    functions problem

---
+++ Basics/C8 Functions/practiceSet/p1.py
@@ -0,0 +1,13 @@
+def greatest(a,b,c):
+    if(a>b and a>c):
+        return a
+    elif(b>a and b>c):
+        return b
+    elif(c>b and c>a):
+        return c
+    
+a=1
+b=3
+c=5
+n=greatest(a,b,c)
+print(n)
