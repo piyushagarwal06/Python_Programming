c8b0112b950c87d54b39df81720e28f8458fc890
piyushagarwal06 <peeyushagerwal720@gmail.com>
2026-03-24 10:41:37 +0530

    f string updated

--- Basics/C3 Strings/practice_set/f_string.py
+++ Basics/C3 Strings/practice_set/f_string.py
@@ -1,2 +1,23 @@
 name= input("Enter your name:")
 print(f"Good Afternoon, {name} ")
+
+
+
+# As we learned in the Python Variables chapter, we cannot combine strings and numbers like this:
+# age = 36
+# #This will produce an error:
+# txt = "My name is John, I am " + age
+# print(txt)
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
+
+
+
+price = 59
+txt = f"The price is {price} dollars"
+print(txt)
+txt = f"The price is {price:.11f} dollars"
+print(txt)
+txt = f"The price is {20 * 59} dollars"
+print(txt)
