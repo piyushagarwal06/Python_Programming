773034e5db1732cc63c4151f7418f78a96b33821
piyushagarwal06 <peeyushagerwal720@gmail.com>
2026-03-25 12:00:29 +0530

    membership operator

---
+++ Basics/C2 Variables and Datatypes/membershipOperators.py
@@ -0,0 +1,15 @@
+# in 	    Returns True if a sequence with the specified value is present in the object	    x in y	
+# not in	Returns True if a sequence with the specified value is not present in the object	x not in y	
+
+fruits = ["apple", "banana", "cherry"]
+print("banana" in fruits)
+
+fruits = ["apple", "banana", "cherry"]
+print("pineapple" not in fruits)
+
+
+text = "Hello World"
+
+print("H" in text)
+print("hello" in text)
+print("z" not in text)
